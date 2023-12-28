1b5235eb70c69517173512d23ec6f23232dfa19d
yuuki <>
2023-12-28 08:56:27 +0000

    fix style & colors

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ fetch('./README.md')
   .then(() => {
     const aTags = document.querySelectorAll('#main a');
     aTags.forEach((aTag) => {
-      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+      const maxColors = (1<<24)|0;
+      const randomColor = `#${Math.floor(Math.random() * maxColors).toString(16).padStart(6, '0')}`;
       aTag.style.color = randomColor;
       aTag.style.textDecoration = 'underline';
       aTag.setAttribute('target', '_blank');

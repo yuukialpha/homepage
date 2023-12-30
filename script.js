ee7b23987fd31d57b4ca7d3a40491f872b83f6db
root <root@vultr.guest>
2023-12-30 09:14:41 +0900

    test

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ fetch('./README.md')
       aTag.setAttribute('target', '_blank');
     });
   })
-  .then(() => {
-    fetch('https://taikoapp.uk/api/songs')
-      .then((res) => res.json())
-      .then((json) => {
-        document.getElementById('taiko-size') = json.length.toString();
-      });
-  });
+
+addEventListener('load', () => {
+  fetch('https://taikoapp.uk/api/songs')
+    .then((res) => res.json())
+    .then((json) => {
+      document.querySelector('#taiko-size').textContent = json.length.toString();
+    });
+});
